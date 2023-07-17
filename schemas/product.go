06db815e94cc863f6940db10e02eabf2591505d2
Tiago Nora <33982771+TiagoNora@users.noreply.github.com>
2023-07-17 02:40:39 +0100

    Add product response

--- schemas/product.go
+++ schemas/product.go
@@ -1,6 +1,8 @@
 package schemas
 
 import (
+	"time"
+
 	"github.com/google/uuid"
 	"github.com/shopspring/decimal"
 	"gorm.io/gorm"
@@ -18,4 +20,15 @@ type Product struct {
 func (product *Product) BeforeCreate(tx *gorm.DB) (err error) {
 	product.Id = uuid.NewString()
 	return
+}
+
+type ProductResponse struct {
+	Id          string          `json:"id"`
+	CreatedAt   time.Time       `json:"createdAt"`
+	UpdatedAt   time.Time       `json:"updatedAt"`
+	DeletedAt   time.Time       `json:"deletedAt,omitempty"`
+	Description string          `json:"description"`
+	Designation string          `json:"designation"`
+	Company     string          `json:"company"`
+	Price       decimal.Decimal `json:"price"`
 }
