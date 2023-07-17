3490a2cc75ce23de97493afec5a9451ac545adcb
Tiago Nora <33982771+TiagoNora@users.noreply.github.com>
2023-07-17 11:59:40 +0100

    Adding the update function

--- handler/request.go
+++ handler/request.go
@@ -35,3 +35,19 @@ func (r *CreateProductRequest) Validate() error {
 	}
 	return nil
 }
+
+type UpdateProductRequest struct {
+	Description string          `json:"description"`
+	Designation string          `json:"designation"`
+	Company     string          `json:"company"`
+	Price       decimal.Decimal `json:"price"`
+}
+
+func (r *UpdateProductRequest) Validate() error {
+	// If any field is provided, validation is truthy
+	if r.Description != "" || r.Designation != "" || r.Company != "" || r.Price.GreaterThan(decimal.NewFromInt(0)) {
+		return nil
+	}
+	// If none of the fields were provided, return falsy
+	return fmt.Errorf("at least one valid field must be provided")
+}
--- handler/updateProduct.go
+++ handler/updateProduct.go
@@ -1,7 +1,57 @@
 package handler
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/TiagoNora/GoCRUD/schemas"
+	"github.com/gin-gonic/gin"
+	"github.com/shopspring/decimal"
+)
 
 func UpdateProduct(ctx *gin.Context) {
+	request := UpdateProductRequest{}
 
+	ctx.BindJSON(&request)
+
+	if err := request.Validate(); err != nil {
+		logger.Errorf("validation error: %v", err.Error())
+		sendError(ctx, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	id := ctx.Query("id")
+	if id == "" {
+		sendError(ctx, http.StatusBadRequest, errParamIsRequired("id", "queryParameter").Error())
+		return
+	}
+	opening := schemas.Product{}
+
+	if err := db.First(&opening, "id = ?", id).Error; err != nil {
+		sendError(ctx, http.StatusNotFound, "opening not found")
+		return
+	}
+	// Update opening
+	if request.Description != "" {
+		opening.Description = request.Description
+	}
+
+	if request.Designation != "" {
+		opening.Designation = request.Designation
+	}
+
+	if request.Company != "" {
+		opening.Company = request.Company
+	}
+
+	if request.Price.GreaterThan(decimal.NewFromInt(0)) {
+		opening.Price = request.Price
+	}
+
+	// Save opening
+	if err := db.Save(&opening).Error; err != nil {
+		logger.Errorf("error updating opening: %v", err.Error())
+		sendError(ctx, http.StatusInternalServerError, "error updating opening")
+		return
+	}
+	sendSuccess(ctx, "update-opening", opening)
 }
